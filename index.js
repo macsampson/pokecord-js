aa5b67a6d9eca246ddb22f2b4e189c2dfbe3d547
Mac <mac.sampson@pm.me>
2021-06-15 10:16:50 -0700

    create event handler

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 require('dotenv').config();
-const pokemon = require('pokemontcgsdk');
-const Discord = require('discord.js');
-const fs = require('fs');
+const pokemon = require('pokemontcgsdk'),
+    Discord = require('discord.js'),
+    fs = require('fs'),
+    config = require('./config.json'),
+    currency_helper = require('./utils/currency_helper');
+// mongoose = require('mongoose'),
+// { User, Shop } = require('./database/dbObjects');
 
+const currency = new Discord.Collection();
 
-
-const prefix = '!';
+currency_helper.execute(currency);
 
 pokemon.configure({ apiKey: process.env.POKEMON_TCG_KEY });
 
 const client = new Discord.Client();
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client, currency));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client, currency));
+    }
+}
+
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
     client.commands.set(command.name, command);
 }
 
-
-client.once('ready', () => {
-    console.log('Pokecord is now online!');
-
-})
-
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command == 'open') {
-
-        client.commands.get('open').execute(message, args).catch(console.error.bind(console));
-
-    }
-})
-
 client.login(process.env.DISCORD_CLIENT_ID)
+
+
+// Connect to the database
+// mongoose.connect(config.mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Connected to MongoDB')
+// }).catch((err) => {
+//     console.log('Unable to connect to MongoDB Database.\nError: ' + err)
+// })
+
+
+
