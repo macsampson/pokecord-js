5aa917d14c918729bd316a7d24731e0a83fcdf32
Mac Sampson <mac.sampson@proton.me>
2024-05-07 17:30:30 -0700

    reformat

--- commands/balance.js
+++ commands/balance.js
@@ -1,14 +1,13 @@
-
 module.exports = {
-    name: "balance",
-    description:
-        "Shows balance of user",
-    usage: "!balance",
-    category: "info",
-    execute(message, args, currency) {
-        // console.log(currency)
-        const target = message.mentions.users.first() || message.author;
-        return message.channel.send(`${target.tag} has ${currency.getBalance(target.id)} 💰`);
-    }
-}
-
+  name: "balance",
+  description: "Shows balance of user",
+  usage: "!balance",
+  category: "info",
+  execute(message, args, currency) {
+    // console.log(currency)
+    const target = message.mentions.users.first() || message.author;
+    return message.channel.send(
+      `${target.tag} has ${currency.getBalance(target.id)} 💰`
+    );
+  },
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+
 const pokemon = require('pokemontcgsdk'),
-    Discord = require('discord.js'),
-    fs = require('fs'),
-    config = require('./config.json'),
-    currency_helper = require('./utils/currency_helper');
-// mongoose = require('mongoose'),
-// { User, Shop } = require('./database/dbObjects');
+	Discord = require('discord.js'),
+	fs = require('fs'),
+	config = require('./config.json'),
+	currency_helper = require('./utils/currency_helper');
+// (mongoose = require('mongoose')),
+// 	({ User, Shop } = require('./database/dbObjects'));
 
 const currency = new Discord.Collection();
 
@@ -14,41 +15,48 @@ currency_helper.execute(currency);
 pokemon.configure({ apiKey: process.env.POKEMON_TCG_KEY });
 
 const client = new Discord.Client();
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client, currency));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args, client, currency));
-    }
-}
-
-
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+const eventFiles = fs
+	.readdirSync('./events')
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) =>
+			event.execute(...args, client, currency)
+		);
+	} else {
+		client.on(event.name, (...args) =>
+			event.execute(...args, client, currency)
+		);
+	}
 }
 
-client.login(process.env.DISCORD_CLIENT_ID)
+const commandFiles = fs
+	.readdirSync('./commands/')
+	.filter((file) => file.endsWith('.js'));
+// console.log(`Loading ${commandFiles.length} commands.`);
+commandFiles.forEach((file) => {
+	// console.log(`Loading command file: ${file}`);
+	const command = require(`./commands/${file}`);
+	// console.log(`Loaded command: ${command.name}`);
+	client.commands.set(command.name, command);
+});
+// console.log(`Registered ${client.commands.size} commands.`);
 
+client.login(process.env.DISCORD_TOKEN);
 
 // Connect to the database
-// mongoose.connect(config.mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(() => {
-//     console.log('Connected to MongoDB')
-// }).catch((err) => {
-//     console.log('Unable to connect to MongoDB Database.\nError: ' + err)
-// })
-
-
-
+// mongoose
+// 	.connect(config.mongoURI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(() => {
+// 		console.log('Connected to MongoDB');
+// 	})
+// 	.catch((err) => {
+// 		console.log('Unable to connect to MongoDB Database.\nError: ' + err);
+// 	});
